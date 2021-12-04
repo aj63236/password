82b39af09cf03f42459632491d98206524ef28fd
aj63236 <59002970+aj63236@users.noreply.github.com>
2021-12-05 02:59:55 +0800

    Debug

--- password.py
+++ password.py
@@ -1,15 +1,12 @@
 password = 'a123456'
-j = 1
-while j < 4:
+j = 0
+while j < 3:
 	j = j + 1
 	pwd = input('請輸入密碼:')
 	if pwd == password:
 		print("登錄成功!")
 		break
-	elif j == 4:
+	elif j == 3:
 		print("錯誤太多次，已拒絕登錄")
 	else:
-		print("密碼錯誤，請再輸入一次密碼，還有%s次機會" %(4-j))
-
-
-
+		print("密碼錯誤，請再輸入一次密碼，還有%s次機會" %(3-j))
